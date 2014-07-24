e7a36336c5970f655013268cbb401c58b783fd8f
Tracy Quintos <tracethesea@gmail.com>
2014-07-23 23:41:12 -0700

    Update main.cpp

--- Hmwk/Assignment5/GaddisChp6Prob13/main.cpp
+++ Hmwk/Assignment5/GaddisChp6Prob13/main.cpp
@@ -6,15 +6,23 @@
  */
 
 #include <iostream>
-#include <iomanip>
-#include <cmath>
+#include <iomanip> //setprecision
+#include <cmath> //for calculations
           
 using namespace std;
 
-float getSpools(float &ordered, float &stock, float &charges);
-void displayOrder(float, float, float);
+//User Libraries
 
-int main ()
+//Global Constants
+
+//Function Prototype
+
+//Execution starts here!
+
+float getSpools(float &ordered, float &stock, float &charges); //getspools function and variables with &
+void displayOrder(float, float, float);                        //displayOrder function and float variables
+
+int main(int argc, char** argv)
 {
 
 float charges, ordered, stock;    
@@ -23,33 +31,34 @@ getSpools(ordered, stock, charges);
 
 displayOrder(ordered, stock, charges);
 
- 
+//exit stage right 
 return 0;
 }
 float getSpools(float &ordered, float &stock, float &charges)
 {
      char   specialChargeYN;
      
-     cout << "The number of spools ordered: ";
+     cout << "The number of spools ordered: "; //user is asked how many spools of copper are ordered
      cin >> ordered;
      
      while (ordered < 1)
            {
                     cout << "\n\nInvalid Input.\n\n\nEnter a number higher than 1: ";
-                    cin >> ordered;
+                    cin >> ordered; //input validation, the number of spools must exceed 1
            }
            
-     cout << "\n\nThe number of spools in stock: ";
+     cout << "\n\nThe number of spools in stock: "; //stock holds the number of spools the company has in stock
      cin >> stock;
      
      while (stock < 0)
            {
                     cout << "\n\nInvalid Input.\n\n\nEnter a number higher than 0: ";
-                    cin >> stock;
+                    cin >> stock; //input validation, the stocked spools must exceed 0
            }
            
      cout << "\n\nAny special shipping and handling (Y/N)? ";
-     cin >> specialChargeYN;
+     cin >> specialChargeYN; //option given by book, gives the user an option to decided whether
+                            //the order has a special charges or not
      
      if ((specialChargeYN == 'Y') || (specialChargeYN == 'y'))
         {
@@ -66,37 +75,37 @@ float getSpools(float &ordered, float &stock, float &charges)
         }
      else
         {
-                    charges = 10.00;
+                    charges = 10.00; //default arguement from book***
         }
 }
-void displayOrder(float ordered, float stock, float charges)
-{
+void displayOrder(float ordered, float stock, float charges) //displayOrder function holds the following variables:
+{                                                           //shipping, perSpool, total cost and special charges
      float backOrder = 0, shipping, perSpool, totalCost, specialCharges;
      
-     cout << "\n\nThe number of spools ready to ship from stock: " << ordered;
+     cout << "\n\nThe number of spools ready to ship from stock: " << ordered; //ordered spools ready for shipment
      
      if (ordered > stock)
         { 
-                 backOrder = abs(stock - ordered);
+                 backOrder = abs(stock - ordered); //in stock is subtracted by the number of spool that user ordered
                  
-                 cout << "\n\nThe number of ordered spools on back order: " << backOrder;
+                 cout << "\n\nThe number of ordered spools on back order: " << backOrder; 
         }
         
         cout << fixed << showpoint << setprecision(2);
         
         shipping = ordered - backOrder;
         
-        perSpool = shipping * 100.00;
+        perSpool = shipping * 100.00; //perSpool is calculted by shiping x $100.00 **copied from book
         
      cout << "\n\nThe total selling price of the portion ready to ship $" << perSpool;
      
-        specialCharges = shipping * charges;
+        specialCharges = shipping * charges; //special charges calculations
      
      cout << "\n\nThe total shipping and handling charges on the portion ready to ship $" << specialCharges;
      
-        totalCost = perSpool + specialCharges;
+        totalCost = perSpool + specialCharges; //total charges calculation
         
-     cout << "\n\nThe total of the order ready to ship $" << totalCost;
+     cout << "\n\nThe total of the order ready to ship $" << totalCost; //totalcost calculated and is given
      
        
 }
