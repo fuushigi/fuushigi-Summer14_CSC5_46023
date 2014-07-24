49ad130d6902ef2187178465e2ac9e3bfe6b29e4
Tracy Quintos <tracethesea@gmail.com>
2014-07-23 23:47:58 -0700

    Update main.cpp

--- Hmwk/Assignment5/GaddisChp6Prob4/main.cpp
+++ Hmwk/Assignment5/GaddisChp6Prob4/main.cpp
@@ -11,16 +11,24 @@
 
 using namespace std;
 
-float lowNum(float,float,float,float,float);
-void display(float);
-void acciN (float &, float &, float &, float &, float &);
+//User Libraries
+
+//Global Constants
+
+//Function Prototype
+
+//Execution starts here!
+
+float lowNum(float,float,float,float,float); //function for accident numbers, holds 5 floats
+void display(float); //displays the low variable
+void acciN (float &, float &, float &, float &, float &); // accident numbers using &
 
 /*
  * 
  */
 int main(int argc, char** argv) {
     
-    float low,n1,s2,e3,w4,c5;
+    float low,n1,s2,e3,w4,c5; //varibles of the regions that will be used to determine lowest accidents
     
     acciN(n1,s2,e3,w4,c5);
     
@@ -28,6 +36,7 @@ int main(int argc, char** argv) {
     
     display(low);
 
+     //exit stage right
      return 0;
 }
 
@@ -43,12 +52,12 @@ void acciN (float &n,float &s,float &e,float &w, float &c)
     
     float lowNum (float n1, float s2, float e3, float w4, float c5)
     {
-        float low=n1;
+        float low=n1; //if statement to compare the accident numbers from regions and gets the lowest number
         if(s2<low)low=s2;
         if(e3<low)low=e3;
         if(w4<low)low=w4;
         if(c5<low)low=c5;
-        return low;
+        return low; //low is returned to the main function
     }
 
 
