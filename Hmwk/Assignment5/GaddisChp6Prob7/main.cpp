1d04f2b615437f5137d5f8b75e3fe27cbe2e49b7
Tracy Quintos <tracethesea@gmail.com>
2014-07-23 23:55:39 -0700

    Update main.cpp

--- Hmwk/Assignment5/GaddisChp6Prob7/main.cpp
+++ Hmwk/Assignment5/GaddisChp6Prob7/main.cpp
@@ -10,11 +10,19 @@
 
 float cvs (int fahrenheit); // function celcius
 
-const int START_FAH = 0,
+const int START_FAH = 0, //constants is taken from a previous assignment, modified it to use a function
           END_FAH = 20,
           INCREMENT = 1;
 
 using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototype
+
+//Execution starts here!
 
 
 /*
@@ -25,16 +33,17 @@ int main(int argc, char** argv) {
     float celcius; 
     cout<<fixed<<showpoint<<setprecision(2);
     
-    for(int fahrenheit = START_FAH; fahrenheit <= END_FAH; fahrenheit ++)
+    for(int fahrenheit = START_FAH; fahrenheit <= END_FAH; fahrenheit ++) //fahrenheit conversion to celcius incremented by 20
     {
         celcius=cvs(fahrenheit);
         cout<<fahrenheit<<" Fahrenheit"<<" = "<<celcius<<" Celcius"<<endl;
     }
 
+    //exit stage right
     return 0;
 }
 
-float cvs (int fahrenheit)
+float cvs (int fahrenheit) //function holds the fahrenheit variable. 
 {
-    return (static_cast<float>(fahrenheit)-32)*5/9;
+    return (static_cast<float>(fahrenheit)-32)*5/9; //calcultes the conversion between fahernheit to
 }
