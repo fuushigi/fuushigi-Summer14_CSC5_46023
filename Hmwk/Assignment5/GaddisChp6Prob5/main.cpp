b00f00d357d3aeb19e16d93a11232229b13e5141
Tracy Quintos <tracethesea@gmail.com>
2014-07-23 23:50:53 -0700

    Update main.cpp

--- Hmwk/Assignment5/GaddisChp6Prob5/main.cpp
+++ Hmwk/Assignment5/GaddisChp6Prob5/main.cpp
@@ -8,9 +8,17 @@
 #include <iostream>
 #include <iomanip>
 
-void cvs (float, float);
+void cvs (float, float); //function for falling distance, holds two floats will be used later
 
 using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototype
+
+//Execution starts here!
 
 
 
@@ -19,22 +27,23 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     
-    float g = 9.8;
-    float t;
+    float g = 9.8; //holds g, given by book
+    float t;     //holds the amonut of time, in seconds
     
     cout<<fixed<<showpoint<<setprecision(2);
     
-    cout<<"Please enter the amount of time"<<endl;
+    cout<<"Please enter the amount of time"<<endl; //user is asked to enter in the time of falling object
     cin>>t;
     cvs(g,t);
 
+    //exit stage right
     return 0;
 }
 
 void cvs (float g, float t)
 {
-    float d;
+    float d; //looking for value of d, we multiplied 1/2 times g times t squared.
     d = 0.5*g*t*t;
-  cout<<"The distance of falling object is: "<<d<< " meters"<<endl;
+  cout<<"The distance of falling object is: "<<d<< " meters"<<endl; //d is given after calculations
 }
 
