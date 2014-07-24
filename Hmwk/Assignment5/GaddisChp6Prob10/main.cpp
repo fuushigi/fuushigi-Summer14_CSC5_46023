519f6ffb1b0765510560c599baeef75dcfed1487
Tracy Quintos <tracethesea@gmail.com>
2014-07-23 23:29:35 -0700

    Update main.cpp

--- Hmwk/Assignment5/GaddisChp6Prob10/main.cpp
+++ Hmwk/Assignment5/GaddisChp6Prob10/main.cpp
@@ -17,27 +17,28 @@ int findLowest(int score1, int score2, int score3, int score4, int score5);
 int main ()
 {
     
-int     score1 = 0, score2 = 0, score3 = 0, score4 = 0, score5 = 0, lowest = 0;
+int     score1 = 0, score2 = 0, score3 = 0, score4 = 0, score5 = 0, lowest = 0; //setting scores 1-5 = 0
     
-        getScore(score1);
+        getScore(score1); // test scores 1-5
         getScore(score2);
         getScore(score3);
         getScore(score4);
         getScore(score5);
 
-        lowest = findLowest(score1, score2, score3, score4, score5);;
-
+        lowest = findLowest(score1, score2, score3, score4, score5);; 
+        //looking for the lowest score out of the 5 test scores that will be given by user
         calcAverage(score1, score2, score3, score4, score5, lowest);
-
+        //first we will add the scores together and subtract it by the lowest score given 
+        //will be averaged out divded by 4
 
 return 0;
 }
-void getScore(int &score)
+void getScore(int &score) //getScore function
 {
      
     // for (int repeat = 0; repeat < 5; repeat++)
          //{
-              cout << "Enter a test score between 0 and 100: ";
+              cout << "Enter a test score between 0 and 100: "; //user must enter in a score from 0 to 100
               cin >> score;
               
               cout << "\n\n";
@@ -45,14 +46,14 @@ void getScore(int &score)
               while ((score < 0) || (score > 100))
                     {
                           cout << "Invalid Input.\n\nEnter a test score between 0 and 100: ";
-                          cin >> score;
+                          cin >> score; //if score does not accomodate given range the program will ask user to re-enter value
               
                           cout << "\n\n";
                     }
          //}                        
 }
 int findLowest(int score1, int score2, int score3, int score4, int score5)
-{
+{                              //findLowest function determines the lowest value through the use of if statements
     int lowest = score1;
     
     if (score2 < lowest)
@@ -74,10 +75,11 @@ int findLowest(int score1, int score2, int score3, int score4, int score5)
        
        cout << "The lowest test score to drop is: " << lowest;
        
-       return lowest;
+       return lowest; //returns the lowest value
 }
 void calcAverage(int score1, int score2, int score3, int score4, int score5, int lowest)
-{
+{ //void function calAverage will take the 5 tests scores and subtract the lowest score
+ //it will be dvided by 4
      float average;
      
      average = ((score1 + score2 + score3 + score4 + score5) - lowest) / 4.0;
