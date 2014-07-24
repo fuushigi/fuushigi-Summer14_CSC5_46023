cbe1e83f1c1c896b6fdf101311817beb07458a0d
Tracy Quintos <tracethesea@gmail.com>
2014-07-23 23:44:37 -0700

    Update main.cpp

--- Hmwk/Assignment5/GaddisChp6Prob21/main.cpp
+++ Hmwk/Assignment5/GaddisChp6Prob21/main.cpp
@@ -9,39 +9,47 @@
 
 using namespace std;
 
-bool IsPrime(int);
+//User Libraries
+
+//Global Constants
+
+//Function Prototype
+
+//Execution starts here!
+
+bool IsPrime(int); //isPrime function given by book, using bool statement
 
 int main(int argc, char** argv)
 {	
 	int number;
 	
-	cout << "Enter an integer that is greater than 1: ";
+	cout << "Enter an integer that is greater than 1: "; //user is asked to enter in a number greater than 1
 	cin >> number;
 	
-	if (IsPrime(number))
+	if (IsPrime(number)) //if statement to determine from function if number given is a prime or not
 	{
-		cout << number << " is prime." << endl;
+		cout << number << " is a prime." << endl;
 	}
 	else
 	{
-		cout << number << " is not prime." << endl;
+		cout << number << " is not a prime." << endl;
 	}
 
-	// This is more convention than anything.
+	//exit stage right
 	return 0;
 }
 
 bool IsPrime(int number)
 {	
-	int i;
+	int i; //holds i
 	
-	for (i=2; i<number; i++)
+	for (i=2; i<number; i++) //for-loop statement to determine if number is a prime or non-prime
 	{
 		if (number % i == 0)
 		{
-			return false;
+			return false; //is not prime
 		}
 	}
 	
-	return true;	
+	return true;	 //is a prime
 }
